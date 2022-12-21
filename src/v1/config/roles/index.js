45c3d4c41123a59fc0fa3e76eb957d20194963d4
Samer A <thedev.samer@gmail.com>
2022-12-21 19:30:08 +0200

    added office role file in roles directory

--- src/v1/config/roles/index.js
+++ src/v1/config/roles/index.js
@@ -1,9 +1,11 @@
 const AccessControl = require("accesscontrol");
 const user = require("./user");
+const office = require("./office");
 const admin = require("./admin");
 
 const roles = new AccessControl({
   user,
+  office,
   admin,
 });
 
