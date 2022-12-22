1f548ea943e348bb99459a11ba153f4a126833eb
Samer A <thedev.samer@gmail.com>
2022-12-22 12:17:25 +0200

    fixed a bug in validation middleware

--- src/v1/middleware/validation/car/rentCar.js
+++ src/v1/middleware/validation/car/rentCar.js
@@ -3,6 +3,10 @@ const errors = require("../../../config/errors");
 const commonMiddleware = require("../common");
 
 const getAllRentCarsValidator = [
+  (req, res, next) => {
+    req.body.skip = req.query.skip;
+    next();
+  },
   commonMiddleware.checkSkip,
   commonMiddleware.next,
 ];
