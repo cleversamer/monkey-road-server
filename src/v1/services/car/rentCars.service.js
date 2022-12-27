d17a10823c980fd8072e05a083db92fbf9553f62
Samer A <thedev.samer@gmail.com>
2022-12-27 13:44:12 +0200

    fixed a bug in rent cars service

--- src/v1/services/car/rentCars.service.js
+++ src/v1/services/car/rentCars.service.js
@@ -1,5 +1,5 @@
 const { RentCar } = require("../../models/car/rentCar.model");
-const { brandsService } = require("../../services");
+const brandsService = require("./brands.service");
 const localStorage = require("../../services/storage/localStorage.service");
 const { ApiError } = require("../../middleware/apiError");
 const httpStatus = require("http-status");
