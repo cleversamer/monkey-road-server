77a60937e5317af6f277896c3d181406a81ecbb9
cleversamerr <thedev.samer@gmail.com>
2023-02-19 20:10:22 +0200

    updated notification class

--- src/v1/config/notifications/Notification.js
+++ src/v1/config/notifications/Notification.js
@@ -1,7 +1,8 @@
 module.exports = class Notificatin {
-  constructor(title = "", body = "", data = {}) {
+  constructor(title = "", body = "", data = {}, photoURL = "") {
     this.title = title;
     this.body = body;
     this.data = data;
+    this.photoURL = photoURL;
   }
 };
