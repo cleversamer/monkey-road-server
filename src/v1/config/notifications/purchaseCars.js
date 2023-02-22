8c56a868dc33c55037f236412b180299a1923182
cleversamerr <thedev.samer@gmail.com>
2023-02-22 12:57:07 +0200

    upated post purchase car notifications

--- src/v1/config/notifications/purchaseCars.js
+++ src/v1/config/notifications/purchaseCars.js
@@ -3,13 +3,17 @@ const Notificatin = require("./Notification");
 module.exports = {
   postAddedForAdmin: (photoURL) =>
     new Notificatin(
+      "New purchase car has been posted to app",
       "تم نشر سيّارة للبيع جديدة الآن",
+      "There is a new purchase car has been posted to app",
       "يوجد هناك سيّارة للبيع جديدة تم نشرها الآن داخل التطبيق",
       photoURL
     ),
   postAddedForUser: (photoURL) =>
     new Notificatin(
+      "Car has been posted successfully",
       "تم نشر السيّارة بنجاح",
+      "Your car has been successfully posted and is now offered for sale within the app",
       "تم نشر سيّارتك بنجاح وهي معروضة الآن للبيع داخل التطبيق",
       photoURL
     ),
