4a7e5b36220bab518b2ada7d58339dcb85e1824e
cleversamerr <thedev.samer@gmail.com>
2023-03-27 00:44:09 +0300

    updated rent order's calcTotalPrice function

--- src/v1/models/car/rentOrder.model.js
+++ src/v1/models/car/rentOrder.model.js
@@ -205,7 +205,7 @@ rentOrderSchema.methods.calcTotalPrice = function (noOfDays, price) {
     const totalPrice = Math.ceil(noOfDays * pricePerDay + deposit);
 
     // Set total price and fatora payment gateway fees (%2.5)
-    this.totalPrice = totalPrice * 1.025;
+    this.totalPrice = Math.ceil(totalPrice * 1.025);
 
     const adminFees = Math.ceil(totalPrice * 0.15);
     const officeBalance = totalPrice - adminFees;
